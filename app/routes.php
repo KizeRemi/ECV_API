5b2afcba26505d3a5e1fc3bdd40743e26ef045c3
KizeRemi <remi.mavillaz@live.fr>
2017-04-06 17:06:03 +0200

    init project

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,12 @@
 <?php
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\DBAL\Query\QueryBuilder;
 
-echo "test";
+$app->get('/', function(Request $request) use ($app){
+  
+  $q = $request->query->get("q");
+  return new JsonResponse(["ok" => $q], 200);
+});
--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@ require_once("../vendor/autoload.php");
 
 use Silex\Application;
 
-$app            = new Application();
+$app = new Application();
 
 $app['debug'] = true;
 require_once("../app/bootstrap.php");
