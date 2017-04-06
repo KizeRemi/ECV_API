3b746d737b0dc670592ed309d5b403105e029640
KizeRemi <remi.mavillaz@live.fr>
2017-04-06 18:02:17 +0200

    client guzzle

--- app/api_routes.php
+++ app/api_routes.php
@@ -3,7 +3,7 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-
+use GuzzleHttp\Client;
 
 // Home
 $app->get('/', function(Request $request) use ($app) {
@@ -13,15 +13,17 @@ $app->get('/', function(Request $request) use ($app) {
 });
 
 // Get query
+// Get query
 $app->get('/{query}', function(Request $request, $query) use ($app) {
 	// Dailymotion
-    $data = json_decode(file_get_contents('https://api.dailymotion.com/videos?tags=' . $query));
-
-    echo 'lol';
-	var_dump($data);
-	die;
-
-	$result = '';
-
+	$client = new Client([
+	    'base_uri' => 'https://api.dailymotion.com',
+	    'timeout'  => 2.0,
+	]);
+	$res = $client->get('/videos', [
+	    'query' => ['tags' => $query]
+	]);
+	$result = json_decode($res->getBody()->__toString());
+	
 	return new JsonResponse($result, 200);
 });
